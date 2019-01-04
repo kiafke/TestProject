cd990ea20010c5157ed8d918367b80e9ab35534b
kiafke <46328435+kiafke@users.noreply.github.com>
2019-01-04 10:39:10 -0500

    AddedInTargetCursor

--- Source/TestProject/TestProjectGameMode.cpp
+++ Source/TestProject/TestProjectGameMode.cpp
@@ -1,10 +1,13 @@
 // Copyright 1998-2018 Epic Games, Inc. All Rights Reserved.
 
 #include "TestProjectGameMode.h"
+#include "MyHUD.h"
 #include "TestProjectPawn.h"
 
 ATestProjectGameMode::ATestProjectGameMode()
 {
 	// set default pawn class to our flying pawn
 	DefaultPawnClass = ATestProjectPawn::StaticClass();
+
+	HUDClass = AMyHUD::StaticClass();
 }
